ac462dd2a201dc0197e21f75a991f2f1cf807f67
RoseJue <2317654783@qq.com>
2022-07-23 23:23:37 +0800

    修改：删除测试注释

--- JoinAndSplit_Demo/MainWindow.xaml.cs
+++ JoinAndSplit_Demo/MainWindow.xaml.cs
@@ -34,7 +34,6 @@ namespace JoinAndSplit_Demo
 
         }
 
-        // 测试注释：开关模型集合
         public ObservableCollection<SwitchModel> SwitchModels { get; set; } = new ObservableCollection<SwitchModel>();
 
         /// <summary>
